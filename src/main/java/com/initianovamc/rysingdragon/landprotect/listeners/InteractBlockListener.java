699fd73ebe24edb11af310ea8589cf8d5a77ba9b
Jamal <Jamalg960@gmail.com>
2016-04-22 13:52:19 -0400

    start of autoclaiming

--- src/main/java/com/initianovamc/rysingdragon/landprotect/listeners/InteractBlockListener.java
+++ src/main/java/com/initianovamc/rysingdragon/landprotect/listeners/InteractBlockListener.java
@@ -8,29 +8,53 @@ import com.initianovamc.rysingdragon.landprotect.utils.ClaimKey;
 import com.initianovamc.rysingdragon.landprotect.utils.Utils;
 import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.meta.ItemEnchantment;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.InteractBlockEvent;
 import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.item.Enchantments;
 import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public class InteractBlockListener {
 
 	@Listener
 	public void onInteract(InteractBlockEvent.Secondary event, @First Player player) {
-		
 		if (event.getCause().containsType(Player.class)) {
 			
 			if (player.getItemInHand().isPresent()) {
+				ItemStack itemStack = player.getItemInHand().get();
+				if (event.getTargetBlock().getState().getType().equals(BlockTypes.ENCHANTING_TABLE)) {
+					int level = player.get(Keys.EXPERIENCE_LEVEL).get();
+					if (level >= 10) {
+						if (itemStack.getItem().equals(ItemTypes.LEATHER_BOOTS)) {
+							event.setCancelled(true);
+							ItemEnchantment ench = new ItemEnchantment(Enchantments.EFFICIENCY, 10);
+							List<ItemEnchantment> list = Arrays.asList(ench);
+							itemStack.offer(Keys.ITEM_ENCHANTMENTS, list);
+							itemStack.offer(Keys.HIDE_ENCHANTMENTS, true);
+							itemStack.offer(Keys.DISPLAY_NAME, Text.of("Claiming boots"));
+							itemStack.offer(Keys.ITEM_LORE, Arrays.asList(Text.of("boots that will claim land as you walk")));
+							itemStack.offer(Keys.ITEM_DURABILITY, 65);
+							player.setItemInHand(itemStack);
+							player.offer(Keys.EXPERIENCE_LEVEL, level - 10);
+						}
+					}
+				}
+				
 				ItemType item = Sponge.getRegistry().getType(ItemType.class, Utils.getClaimInspectTool()).get();
-				if (player.getItemInHand().get().getItem().getType().equals(item)) {
+				if (itemStack.getItem().getType().equals(item)) {
 					ClaimKey key = new ClaimKey(player.getWorld().getUniqueId(), player.getLocation().getChunkPosition());
 					if (Utils.claimBoundaries.containsKey(key)) {
 						ClaimBoundary boundary = Utils.claimBoundaries.get(key);
--- src/main/java/com/initianovamc/rysingdragon/landprotect/listeners/PlayerMoveListener.java
+++ src/main/java/com/initianovamc/rysingdragon/landprotect/listeners/PlayerMoveListener.java
@@ -1,16 +1,28 @@
 package com.initianovamc.rysingdragon.landprotect.listeners;
 
 import com.flowpowered.math.vector.Vector3i;
+import com.initianovamc.rysingdragon.landprotect.database.LandProtectDB;
+import com.initianovamc.rysingdragon.landprotect.utils.PlayerClaim;
 import com.initianovamc.rysingdragon.landprotect.utils.Utils;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.meta.ItemEnchantment;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.DisplaceEntityEvent;
+import org.spongepowered.api.item.Enchantments;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.service.permission.Subject;
+import org.spongepowered.api.service.permission.option.OptionSubject;
 import org.spongepowered.api.service.user.UserStorageService;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class PlayerMoveListener {
@@ -23,6 +35,48 @@ public class PlayerMoveListener {
 		UUID worldUUID = player.getWorld().getUniqueId();
 		
 		if (!oldChunk.equals(newChunk)) {
+			
+			if (!Utils.isClaimed(newChunk, worldUUID)) {
+				Optional<ItemStack> optItem = player.getBoots();
+				if (optItem.isPresent()) {
+					if (optItem.get().getItem().equals(ItemTypes.LEATHER_BOOTS) && optItem.get().get(Keys.ITEM_ENCHANTMENTS).isPresent()) {
+						ItemStack i = optItem.get();
+						List<ItemEnchantment> list = i.get(Keys.ITEM_ENCHANTMENTS).get();
+						if (list.get(0).getLevel() == 10 && list.get(0).getEnchantment().equals(Enchantments.EFFICIENCY)) {
+							
+							Subject subject = player;
+							if (subject instanceof OptionSubject) {
+								OptionSubject optSubject = (OptionSubject) subject;
+								int claimLimit = Integer.parseInt(optSubject.getOption("claimlimit").orElse("0"));
+								try {
+									int claimAmount = LandProtectDB.getPlayerClaimAmount(player.getUniqueId());
+									if (claimLimit != 0) {
+										if (claimAmount >= claimLimit) {
+											player.sendMessage(Text.of(TextColors.RED, "You have reached the max claim limit"));
+											return;
+										} 
+									}
+								} catch (SQLException e) {
+									e.printStackTrace();
+								}
+							}
+							
+							
+							int durabilityToRemove = (int) (65 * 0.05);
+							int durability = i.get(Keys.ITEM_DURABILITY).get() - durabilityToRemove;
+							i.offer(Keys.ITEM_DURABILITY, durability);
+							player.setBoots(i);
+							LandProtectDB.addPlayerClaim(new PlayerClaim(worldUUID, newChunk, player.getUniqueId()));
+							player.sendMessage(Text.of("claimed this chunk"));
+							if (durability <= 0) {
+								player.setBoots(null);
+								return;
+							}
+							return;
+						}
+					}
+				}
+			}
 			
 			if (Utils.isClaimed(oldChunk, worldUUID) && !Utils.isClaimed(newChunk, worldUUID)) {
 				player.sendMessage(Text.of(TextColors.DARK_AQUA, "Now entering unclaimed land"));
