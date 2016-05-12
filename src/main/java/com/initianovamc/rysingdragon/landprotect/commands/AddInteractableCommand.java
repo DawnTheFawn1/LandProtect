44e2c4670308f20da68b1c40539f69d36249de68
Jamal <Jamalg960@gmail.com>
2016-05-11 21:31:48 -0400

    addinteractable improvements

--- src/main/java/com/initianovamc/rysingdragon/landprotect/commands/AddInteractableCommand.java
+++ src/main/java/com/initianovamc/rysingdragon/landprotect/commands/AddInteractableCommand.java
@@ -22,38 +22,37 @@ public class AddInteractableCommand implements CommandExecutor{
 
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-
-		if (src instanceof Player) {
-			Player player = (Player)src;
-			
-			if (args.hasAny("block-id")) {
-				String id = (String)args.getOne("block-id").get();
-				if (!Sponge.getRegistry().getType(BlockType.class, id).isPresent()) {
-					player.sendMessage(Text.of(TextColors.RED, "That is not a correct block id"));
-					return CommandResult.success();
-				}
-				
-				try {
-					List<String> interactables = GeneralConfig.getConfig().getConfigNode().getNode("Interactable").getList(TypeToken.of(String.class), new ArrayList<>());
-					if (interactables.contains(id)) {
-						player.sendMessage(Text.of(TextColors.RED, "That block id is already added"));
-						return CommandResult.empty();
-					}
-					
-					interactables.add(id);
-					GeneralConfig.getConfig().getConfigNode().getNode("Interactable").setValue(interactables);
-					GeneralConfig.getConfig().save();
-					player.sendMessage(Text.of(TextColors.GOLD, id, TextColors.DARK_AQUA, " has been added as interactable"));
-				} catch (ObjectMappingException e) {
-					e.printStackTrace();
-				}
-			} else {
-				Utils.inAddInteractMode.add(player.getUniqueId());
-				player.sendMessage(Text.of(TextColors.DARK_AQUA, "Right click a block to add it as interactable"));
+		
+		if (args.hasAny("block-id")) {
+			String id = (String)args.getOne("block-id").get();
+			if (!Sponge.getRegistry().getType(BlockType.class, id).isPresent()) {
+				src.sendMessage(Text.of(TextColors.RED, "That is not a correct block id"));
+				return CommandResult.empty();
 			}
 			
+			try {
+				List<String> interactables = GeneralConfig.getConfig().getConfigNode().getNode("Interactable").getList(TypeToken.of(String.class), new ArrayList<>());
+				if (interactables.contains(id)) {
+					src.sendMessage(Text.of(TextColors.RED, "That block id is already added"));
+					return CommandResult.empty();
+				}
+				
+				interactables.add(id);
+				GeneralConfig.getConfig().getConfigNode().getNode("Interactable").setValue(interactables);
+				GeneralConfig.getConfig().save();
+				src.sendMessage(Text.of(TextColors.GOLD, id, TextColors.DARK_AQUA, " has been added as interactable"));
+				return CommandResult.success();
+			} catch (ObjectMappingException e) {
+				e.printStackTrace();
+			}
+			
+		} else if (src instanceof Player) {
+			Player player = (Player)src;
+			Utils.inAddInteractMode.add(player.getUniqueId());
+			player.sendMessage(Text.of(TextColors.DARK_AQUA, "Right click a block to add it as interactable"));
+			return CommandResult.success();
 		}
-		
+
 		return CommandResult.success();
 	}
 
