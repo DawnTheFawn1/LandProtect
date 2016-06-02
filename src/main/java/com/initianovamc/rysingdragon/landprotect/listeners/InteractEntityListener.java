a08b54539be866119fff92426b651286d426c35e
Jamal <Jamalg960@gmail.com>
2016-06-01 21:57:08 -0400

    actually fully implement protection of frames in claims

--- src/main/java/com/initianovamc/rysingdragon/landprotect/listeners/InteractEntityListener.java
+++ src/main/java/com/initianovamc/rysingdragon/landprotect/listeners/InteractEntityListener.java
@@ -1,19 +1,30 @@
 package com.initianovamc.rysingdragon.landprotect.listeners;
 
-import com.initianovamc.rysingdragon.landprotect.LandProtect;
+import com.flowpowered.math.vector.Vector3i;
+import com.initianovamc.rysingdragon.landprotect.utils.Utils;
 import org.spongepowered.api.entity.hanging.Hanging;
-import org.spongepowered.api.entity.hanging.ItemFrame;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.entity.AttackEntityEvent;
-import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.InteractEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.world.World;
 
 public class InteractEntityListener {
 
 	@Listener
-	public void onInteract(InteractEntityEvent event) {
+	public void onInteract(InteractEntityEvent event, @First Player player) {
 		if (event.getTargetEntity() instanceof Hanging) {
-			event.setCancelled(true);
+			World world = player.getWorld();
+			Vector3i chunk = player.getLocation().getChunkPosition();
+			if (Utils.isAdminClaimed(chunk, world.getUniqueId()) && !player.hasPermission("landprotect.adminclaim.bypass")) {
+				event.setCancelled(true);
+			} else if (Utils.isPlayerClaimed(chunk, world.getUniqueId())) {
+				if (!Utils.getClaimOwner(chunk, world.getUniqueId()).get().equals(player.getUniqueId())) {
+					if (!Utils.isFriend(Utils.getClaimOwner(chunk, world.getUniqueId()).get(), player.getUniqueId()) && !Utils.isTrustedToClaim(chunk, player.getUniqueId(), world.getUniqueId())) {
+						event.setCancelled(true);
+					}
+				}
+			}
 		}
 	}
 }
